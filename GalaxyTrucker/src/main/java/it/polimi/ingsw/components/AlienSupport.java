57c0ff30e4a582d7928ec418749718d19d34b4ee
Carlo Maggi <tempo.drago@gmail.com>
2025-03-14 12:52:55 +0100

    Aggiunta di 2 componenti e definizione dell'enumerazione

---
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/components/AlienSupport.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.components;
+//not finished
+
+/**
+ * Class that represents an alien support component
+ * @author carlo
+ */
+
+public class AlienSupport extends Component{
+
+
+}
---
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/components/Component.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw.components;
+
+/**
+ * Superclass to express what all components have in common
+ * SideType[0] corresponds to the front side, then rotate clockwise.
+ * @author carlo
+ */
+
+public class Component {
+
+    /**
+     *Class constructor
+     * @param sides
+     */
+
+}
