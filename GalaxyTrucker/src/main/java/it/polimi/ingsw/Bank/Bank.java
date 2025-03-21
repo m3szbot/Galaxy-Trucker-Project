80aad26a9112d3d80df08b09e760ae7e5aa92b3e
mbpolimi <10874705@polimi.it>
2025-03-21 20:48:44 +0100

    comment bank class

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/Bank/Bank.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/Bank/Bank.java
@@ -2,9 +2,15 @@ package it.polimi.ingsw.Bank;
 
 import it.polimi.ingsw.Application.GameType;
 
+/**
+ * Bank class used to keep count of game inventory
+ *
+ * @author Boti
+ */
+
 public class Bank {
     // constants (physical quantity)
-    // red yellow green blue
+    // Goods: red yellow green blue
     public final int[] maxGoods = {12, 17, 13, 14};
     public final int maxCredits = 150;
     public final int maxBatteries = 40;
@@ -20,9 +26,12 @@ public class Bank {
     private int purpleAliensNumber;
     private int brownAliensNumber;
 
-    // constructor
-    // attributes are not final to keep count of the quantities in the bank
-    // pass aliens=0 if no aliens are used
+    /**
+     * Constructor
+     * Aliens' number depends on game type
+     *
+     * @param gameType Type of game
+     */
     public Bank(GameType gameType) {
         this.goodsNumber = new int[maxGoods.length];
         System.arraycopy(maxGoods, 0, this.goodsNumber, 0, maxGoods.length);
@@ -38,7 +47,12 @@ public class Bank {
         }
     }
 
-    // removers
+    /**
+     * Remove goods from bank inventory
+     *
+     * @param goods Array of the 4 good types containing the quantities to remove
+     * @return returned Array of quantity violations of each good type - change to exceptions
+     */
     public boolean[] removeGoods(int[] goods) {
         boolean[] returned = {true, true, true, true};
         for (int i = 0; i < 4; i++) {
@@ -50,6 +64,12 @@ public class Bank {
         return returned;
     }
 
+    /**
+     * Remove credits from bank inventory
+     *
+     * @param credits number of credits to remove from bank inventory
+     * @return false if quantity violated - change to exception
+     */
     public boolean removeCredits(int credits) {
         if (this.creditsNumber - credits < 0)
             return false;
@@ -57,6 +77,12 @@ public class Bank {
         return true;
     }
 
+    /**
+     * Remove batteries from bank inventory
+     *
+     * @param batteries number of batteries to remove from bank inventory
+     * @return false if quantity violated - exception
+     */
     public boolean removeBatteries(int batteries) {
         if (this.batteriesNumber - batteries < 0)
             return false;
@@ -64,6 +90,12 @@ public class Bank {
         return true;
     }
 
+    /**
+     * Remove astronauts from bank inventory
+     *
+     * @param astronauts number of astronauts to remove from bank inventory
+     * @return false if quantity violated - exception
+     */
     public boolean removeAstronauts(int astronauts) {
         if (this.astronautsNumber - astronauts < 0)
             return false;
@@ -71,6 +103,12 @@ public class Bank {
         return true;
     }
 
+    /**
+     * Remove purple aliens from bank inventory
+     *
+     * @param purpleAliens number of purple aliens to remove from bank inventory
+     * @return false if quantity violated - exception
+     */
     public boolean removePurpleAliens(int purpleAliens) {
         if (this.purpleAliensNumber - purpleAliens < 0)
             return false;
@@ -78,6 +116,12 @@ public class Bank {
         return true;
     }
 
+    /**
+     * Remove brown aliens from bank inventory
+     *
+     * @param brownAliens number of brown aliens to remove from bank inventory
+     * @return false if quantity violated
+     */
     public boolean removeBrownAliens(int brownAliens) {
         if (this.brownAliensNumber - brownAliens < 0)
             return false;
@@ -85,29 +129,58 @@ public class Bank {
         return true;
     }
 
-    // adders
+    /**
+     * Add goods to bank inventory
+     *
+     * @param goods Array of number of goods to add to bank inventory
+     */
     public void addGoods(int[] goods) {
         for (int i = 0; i < 4; i++) {
             this.goodsNumber[i] += goods[i];
         }
     }
 
+    /**
+     * Add credits to bank inventory
+     *
+     * @param credits Number of credits to add to bank inventory
+     */
     public void addCredits(int credits) {
         this.creditsNumber += credits;
     }
 
+    /**
+     * Add batteries to bank inventory
+     *
+     * @param batteries Number of batteries to add to bank inventory
+     */
     public void addBatteries(int batteries) {
         this.batteriesNumber += batteries;
     }
 
+    /**
+     * Add astronauts to bank inventory
+     *
+     * @param astronauts Number of astronauts to add to bank inventory
+     */
     public void addAstronauts(int astronauts) {
         this.astronautsNumber += astronauts;
     }
 
+    /**
+     * Add purple aliens to bank inventory
+     *
+     * @param purpleAliens Number of purpleAliens to add to bank inventory
+     */
     public void addPurpleAliens(int purpleAliens) {
         this.purpleAliensNumber += purpleAliens;
     }
 
+    /**
+     * Add brown aliens to bank inventory
+     *
+     * @param brownAliens Number of brownAliens to add to bank inventory
+     */
     public void addBrownAliens(int brownAliens) {
         this.brownAliensNumber += brownAliens;
     }
