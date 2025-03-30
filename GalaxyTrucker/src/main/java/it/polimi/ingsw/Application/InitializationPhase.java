573337fdb8a8e205ea2cdeac6595414c29d1e078
LudoMeloni <ludovico.meloni@mail.polimi.it>
2025-03-30 12:21:44 +0200

    Sistemazione SetUpPhase e InitializationPhase

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/Application/InitializationPhase.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/Application/InitializationPhase.java
@@ -1,9 +1,21 @@
 package it.polimi.ingsw.Application;
 
+/**
+ * initialization controller class (calls the initialization methods)
+ *
+ * @author Ludo
+ */
+
 import java.io.IOException;
 
 public class InitializationPhase {
 
+    /**
+     * start is to be called in the main in order to initialize the objects of the game
+     *
+     * @param gameInformation
+     * @throws IOException
+     */
     public void start(GameInformation gameInformation) throws IOException {
         gameInformation.setUpCards(gameInformation.getGameType());
         gameInformation.setUpBank();
--- GalaxyTrucker/src/main/java/it/polimi/ingsw/Application/SetUpPhase.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/Application/SetUpPhase.java
@@ -1,35 +1,61 @@
 package it.polimi.ingsw.Application;
 
+/**
+ * setup controller class (calls the setup methods)
+ *
+ * @author Ludo
+ */
+
 import it.polimi.ingsw.shipboard.Player;
 
 public class SetUpPhase {
     Player player;
 
-    public void SetUpPhase(Player player) {
-    }
-
+    /**
+     * start is the method to be called from main in order to set up the game, it operates differently based on who calls it (the first player or the subsequent ones)
+     *
+     * @param gameInformation
+     */
     public void start(GameInformation gameInformation) {
         if (gameInformation.getPlayerList().isEmpty()) {
-            int maxNumberOfPlayers = askMaxNumberOfPlayers();
+            int maxNumberOfPlayers = askMaxNumberOfPlayers(setUpView);
             gameInformation.setUpPlayers(player, maxNumberOfPlayers);
-            GameType gameType = askGameType();
+            GameType gameType = askGameType(setUpView);
+            gameInformation.setGameType(gameType);
         } else {
             gameInformation.addPlayers(player);
         }
         ViewType viewType = askViewType(setUpView);
-        gameInformation.setGameType(gameType);
         gameInformation.setViewType(viewType);
 
     }
 
+    /**
+     * calls a method in setUpView that asks the first user the max # players
+     *
+     * @param setUpView
+     * @return int
+     */
     public int askMaxNumberOfPlayers(SetUpView setUpView) {
         return setUpView.askMaxNumberOfPlayers();
     }
 
+    /**
+     * calls a method in setUpView that asks the first user the game type
+     *
+     * @param setUpView
+     * @return GameType
+     */
     public GameType askGameType(SetUpView setUpView) {
         return setUpView.askGameType();
     }
 
+    /**
+     * calls a method in setUpView that asks every user the view Type
+     *
+     * @param setUpView
+     * @return viewType
+     */
     public ViewType askViewType(SetUpView setUpView) {
         return setUpView.askViewType();
     }
