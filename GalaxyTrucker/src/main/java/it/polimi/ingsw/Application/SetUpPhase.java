6a31b339da7b03a9a2711afaaa4ef626f4a4a106
LudoMeloni <ludovico.meloni@mail.polimi.it>
2025-04-01 19:27:27 +0200

    SetUpPhase, InitializationPhase e SetUpView

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/Application/SetUpPhase.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/Application/SetUpPhase.java
@@ -17,7 +17,7 @@ public class SetUpPhase implements Startable {
      *
      * @param gameInformation
      */
-    public void start(GameInformation gameInformation, SetUpView setUpView, Player player) throws IOException {
+    public void start(GameInformation gameInformation, SetUpView setUpView) throws IOException {
         if (gameInformation.getPlayerList().isEmpty()) {
             int maxNumberOfPlayers = setUpView.askMaxNumberOfPlayers();
             gameInformation.setUpPlayers(player, maxNumberOfPlayers);
@@ -28,36 +28,6 @@ public class SetUpPhase implements Startable {
         }
         ViewType viewType = setUpView.askViewType();
         gameInformation.setViewType(viewType);
-    }
-
-    /**
-     * calls a method in setUpView that asks the first user the max # players
-     *
-     * @param setUpView
-     * @return int
-     */
-    public int askMaxNumberOfPlayers(SetUpView setUpView) throws IOException {
-        return setUpView.askMaxNumberOfPlayers();
-    }
-
-    /**
-     * calls a method in setUpView that asks the first user the game type
-     *
-     * @param setUpView
-     * @return GameType
-     */
-    public GameType askGameType(SetUpView setUpView) {
-        return setUpView.askGameType();
-    }
-
-    /**
-     * calls a method in setUpView that asks every user the view Type
-     *
-     * @param setUpView
-     * @return viewType
-     */
-    public ViewType askViewType(SetUpView setUpView) {
-        return setUpView.askViewType();
     }
 
 }
