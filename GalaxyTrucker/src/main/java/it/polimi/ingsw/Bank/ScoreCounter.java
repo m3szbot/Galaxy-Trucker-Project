13f73916c75f53de0ddd9bd568e5aee5df4c9881
Botond Meszaros <10874705@polimi.it>
2025-04-04 18:19:54 +0200

    finish scoreCounter

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/Bank/ScoreCounter.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/Bank/ScoreCounter.java
@@ -12,9 +12,11 @@ import it.polimi.ingsw.Shipboard.Player;
  */
 
 public class ScoreCounter {
+    // map of player scores
+    private Map<Player, Integer> playerScoresMap;
+
     // more players can have the minimum and both receive points
     private List<Player> leastExposedLinksList;
-
     // constants
     private final int leastExposedLinksPoints;
     private final int lostComponentsPoints;
@@ -22,13 +24,13 @@ public class ScoreCounter {
     private final int[] goodsPoints;
 
     /**
-     * Constructor, calculates leastExposedLinks player(s)
+     * Constructor, populates playerScoresMap
      *
-     * @param gameType   Game type that determines scoring
-     * @param playerList List of players
+     * @param gameType        Game type that determines scoring
+     * @param playerOrderList List of player orders from FlightBoard!
      */
-    public ScoreCounter(GameType gameType, List<Player> playerList) {
-        // set scoring attributes based on game type
+    public ScoreCounter(GameType gameType, List<Player> playerOrderList) {
+        // set scoring based on game type
         // normal game values
         int leastExposedLinksPoints = 4;
         int lostComponentsPoints = -1;
@@ -40,16 +42,53 @@ public class ScoreCounter {
             finishOrderPoints = new int[]{4, 3, 2, 1};
             goodsPoints = new int[]{4, 3, 2, 1};
         }
-        // set  instance attributes
+        // set scoring attributes
         this.leastExposedLinksPoints = leastExposedLinksPoints;
         this.lostComponentsPoints = lostComponentsPoints;
         this.finishOrderPoints = new int[finishOrderPoints.length];
-        System.arraycopy(finishOrderPoints, 0, finishOrderPoints, 0, finishOrderPoints.length);
+        System.arraycopy(finishOrderPoints, 0, this.finishOrderPoints, 0, finishOrderPoints.length);
         this.goodsPoints = new int[goodsPoints.length];
-        System.arraycopy(goodsPoints, 0, goodsPoints, 0, goodsPoints.length);
+        System.arraycopy(goodsPoints, 0, this.goodsPoints, 0, goodsPoints.length);
         // create and fill leastExposedLinksList with players
-        this.leastExposedLinksList = new ArrayList<Player>();
-        calculateLeastExposedLinks(playerList);
+        this.leastExposedLinksList = new ArrayList<>();
+        calculateLeastExposedLinks(playerOrderList);
+        // populate playerScoresMap
+        this.playerScoresMap = new HashMap<>();
+        for (Player player : playerOrderList) {
+            playerScoresMap.put(player, calculatePlayerScore(player, playerOrderList));
+        }
+    }
+
+    /**
+     * Return score of given player
+     *
+     * @param player Player to examine
+     * @return Score of given player
+     */
+    public int getPlayerScore(Player player) {
+        return playerScoresMap.get(player);
+    }
+
+    /**
+     * Populate leastExposedLinksList with players
+     *
+     * @param playerOrderList List of players
+     */
+    private void calculateLeastExposedLinks(List<Player> playerOrderList) {
+        // Map: player - player exposed links
+        Map<Player, Integer> playerExposedLinks = new Hashtable<>();
+        // calculate exposed links of player and add to temporary Map
+        for (Player player : playerOrderList) {
+            playerExposedLinks.put(player, player.getShipBoard().countExternalJunctions());
+        }
+        // find minimum value
+        int minValue = playerExposedLinks.values().stream().min(Integer::compare).orElse(Integer.MAX_VALUE);
+        // add minimum player to leastExposedLinksList
+        for (Map.Entry<Player, Integer> entry : playerExposedLinks.entrySet()) {
+            if (entry.getValue() == minValue) {
+                leastExposedLinksList.add(entry.getKey());
+            }
+        }
     }
 
     /**
@@ -58,12 +97,27 @@ public class ScoreCounter {
      * @param player Player whose score is to be calculated
      * @return Score of the given player
      */
-    public int calculatePlayerScore(Player player) {
+    private int calculatePlayerScore(Player player, List<Player> playerOrderList) {
         int playerPoints = 0;
+        playerPoints += calculateFinishOrderPoints(player, playerOrderList);
         playerPoints += calculateLeastExposedLinksPoints(player);
         playerPoints += calculateGoodsPoints(player);
         playerPoints += calculateLostComponentsPoints(player);
         return playerPoints;
+    }
+
+    /**
+     * Calculate player score based on finish position
+     * no points if DNF
+     *
+     * @param player          Player to examine
+     * @param playerOrderList Finish order of players
+     * @return Score of player given for finish position
+     */
+    private int calculateFinishOrderPoints(Player player, List<Player> playerOrderList) {
+        int finishPoints = 0;
+        finishPoints += finishOrderPoints[playerOrderList.indexOf(player)];
+        return finishPoints;
     }
 
     /**
@@ -86,47 +140,24 @@ public class ScoreCounter {
      * @return Earned points
      */
     private int calculateGoodsPoints(Player player) {
-        int storagePoints = 0;
-        // TODO
-        // for(int goods : player.shipBoard.)
-        return storagePoints;
+        int goodsPoints = 0;
+        for (int i = 0; i < this.goodsPoints.length; i++) {
+            goodsPoints += this.goodsPoints[i] * player.getShipBoard().getShipBoardAttributes().getGoods()[i];
+        }
+        return goodsPoints;
     }
 
     /**
-     * Calculate earned (negative) points given for lost components
+     * Calculate (negative) points given for lost components
      *
      * @param player Player to evaluate
      * @return Earned (negative) points
      */
     private int calculateLostComponentsPoints(Player player) {
         int lostPoints = 0;
-        // TODO
-        // lostPoints -= player.shipBoard.
+        lostPoints += lostComponentsPoints * player.getShipBoard().getShipBoardAttributes().getDestroyedComponents();
         return lostPoints;
     }
-
-    /**
-     * Add players with the least exposed links to leastExposedLinks list
-     *
-     * @param playerList List of players
-     */
-    private void calculateLeastExposedLinks(List<Player> playerList) {
-        // Map of exposed links of players
-        Map<Player, Integer> playerExposedLinks = new Hashtable<Player, Integer>();
-        // calculate exposed links and add to Map
-        // TODO
-        //for (Player player : playerList) {
-        //playerExposedLinks.put(player, player.shipBoard.)
-        //}
-
-        // find minimum value
-        int minValue = playerExposedLinks.values().stream().min(Integer::compare).orElse(Integer.MAX_VALUE);
-        // add player to leastExposedLinksList
-        for (Map.Entry<Player, Integer> entry : playerExposedLinks.entrySet()) {
-            if (entry.getValue() == minValue) {
-                leastExposedLinksList.add(entry.getKey());
-            }
-        }
-    }
 }
+
 
