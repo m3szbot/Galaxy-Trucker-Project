2f3e9b6da3142eba106c92421b020efea5de48bb
GeckyMerlo <giacomo.merlo@mail.polimi.it>
2025-04-04 17:43:40 +0200

    AssemblyControlUpdate

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/Application/ComponentChoice.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/Application/ComponentChoice.java
@@ -2,34 +2,32 @@ package it.polimi.ingsw.Application;
 import it.polimi.ingsw.Assembly.AssemblyProtocol;
 import it.polimi.ingsw.Shipboard.*;
 
-import javax.swing.text.View;
-import java.util.ArrayList;
-
 public class ComponentChoice implements GameState{
     private AssemblyProtocol assemblyProtocol;
     private Player player;
     private AssemblyView view;
 
 
-    public ComponentChoice(AssemblyView view, AssemblyProtocol protocol) {
+    public ComponentChoice(AssemblyView view, AssemblyProtocol protocol, Player player) {
         this.assemblyProtocol = protocol;
         this.view = view;
+        this.player = player;
     }
 
     @Override
-    public void enter(Game game, AssemblyView view) {
+    public void enter(AssemblyGame assemblyGame, AssemblyView view) {
         view.printComponentChoice();
     }
 
     @Override
-    public void handleInput(String input, Game game) {
+    public void handleInput(String input, AssemblyGame assemblyGame) {
         String imput = input.toLowerCase();
         int caseManagement = -1;
         try {caseManagement = Integer.parseInt(input);}
         catch(NumberFormatException e) {
             e.printStackTrace();
         }
-        if(caseManagement >= 0 && caseManagement < game.getGameInformation().getAssemblyProtocol().getUncoveredList().size()){
+        if(caseManagement >= 0 && caseManagement < assemblyGame.getGameInformation().getAssemblyProtocol().getUncoveredList().size()){
             caseManagement = 1;
         }
         else{
@@ -37,13 +35,15 @@ public class ComponentChoice implements GameState{
         }
         switch (caseManagement) {
             case 1:
-                game.getGameInformation().getAssemblyProtocol().chooseComponent(player, Integer.parseInt(input));
+                view.printUncoveredComponentsMessage(assemblyGame.getGameInformation());
+                assemblyGame.getGameInformation().getAssemblyProtocol().chooseComponent(player, Integer.parseInt(input));
+                view.printNewComponentMessage(assemblyGame.getGameInformation().getAssemblyProtocol().getViewMap().get(player));
                 break;
             case 2:
-                System.out.println("Error in component choice");
+                view.printErrorComponentChoiceMessage();
                 break;
         }
-        game.setState(new AssemblyState(view, assemblyProtocol));
+        assemblyGame.setState(new AssemblyState(view, assemblyProtocol, player));
     }
 
 }
--- GalaxyTrucker/src/main/java/it/polimi/ingsw/Application/GameState.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/Application/GameState.java
@@ -3,7 +3,7 @@ package it.polimi.ingsw.Application;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public interface GameState {
-    void enter(Game game, AssemblyView assemblyView);
-    void handleInput(String input, Game game);
-    default void update(Game game) {} // opzionale, può essere vuoto
+    void enter(AssemblyGame assemblyGame, AssemblyView assemblyView);
+    void handleInput(String input, AssemblyGame assemblyGame);
+    default void update(AssemblyGame assemblyGame) {} // opzionale, può essere vuoto
 }
