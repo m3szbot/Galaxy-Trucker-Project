b1d1079169b4163e24ab39ebf8803db2823576ba
LudoMeloni <ludovico.meloni@mail.polimi.it>
2025-04-15 09:34:44 +0200

    Components Package test aggiunto

--- GalaxyTrucker/src/test/java/it/polimi/ingsw/ShipBoardTest.java
+++ GalaxyTrucker/src/test/java/it/polimi/ingsw/ShipBoardTest.java
@@ -1,25 +1,28 @@
 package it.polimi.ingsw;
+
 import it.polimi.ingsw.Application.GameType;
 import it.polimi.ingsw.Components.*;
 import it.polimi.ingsw.Shipboard.ShipBoard;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ShipBoardTest {
     ShipBoard shipBoard;
+
     @BeforeEach
     void setUp() {
         shipBoard = new ShipBoard(GameType.NormalGame);
-        for(int i = 0; i < 12; i++){
-            for(int j = 0; j < 12; j++){
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 12; j++) {
                 System.out.print(shipBoard.getMatr()[i][j] + "");
             }
             System.out.println();
         }
-        for(int i = 0; i < 12; i++){
-            for(int j = 0; j < 12; j++){
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 12; j++) {
                 System.out.print(shipBoard.getStructureMatrix()[i][j] + "");
             }
             System.out.println();
@@ -27,8 +30,8 @@ public class ShipBoardTest {
     }
 
     @Test
-    void addComponent(){
-        shipBoard.addComponent(new Engine( new SideType[]{SideType.Universal, SideType.Universal, SideType.Special, SideType.Universal}, true), 7, 8);
+    void addComponent() {
+        shipBoard.addComponent(new Engine(new SideType[]{SideType.Universal, SideType.Universal, SideType.Special, SideType.Universal}, true), 7, 8);
         assertEquals(shipBoard.getShipBoardAttributes().getDrivingPower(), 1);
         shipBoard.removeComponent(7, 8);
         assertEquals(shipBoard.getShipBoardAttributes().getDrivingPower(), 0);
@@ -37,18 +40,17 @@ public class ShipBoardTest {
     }
 
 
-
     @Test
-    void addComponent2(){
-        shipBoard.addComponent(new Cannon( new SideType[]{SideType.Special, SideType.Universal, SideType.Universal, SideType.Universal}, true), 7, 8);
+    void addComponent2() {
+        shipBoard.addComponent(new Cannon(new SideType[]{SideType.Special, SideType.Universal, SideType.Universal, SideType.Universal}, true), 7, 8);
         assertEquals(shipBoard.getShipBoardAttributes().getFirePower(), 1);
         shipBoard.removeComponent(7, 8);
         assertEquals(shipBoard.getShipBoardAttributes().getFirePower(), 0);
     }
 
     @Test
-    void addComponent3(){
-        shipBoard.addComponent(new Shield( new SideType[]{SideType.Universal, SideType.Special, SideType.Special, SideType.Universal}), 7, 8);
+    void addComponent3() {
+        shipBoard.addComponent(new Shield(new SideType[]{SideType.Universal, SideType.Special, SideType.Special, SideType.Universal}), 7, 8);
         assertEquals(shipBoard.getShipBoardAttributes().checkSide(0), false);
         assertEquals(shipBoard.getShipBoardAttributes().checkSide(1), true);
         assertEquals(shipBoard.getShipBoardAttributes().checkSide(2), true);
@@ -61,15 +63,15 @@ public class ShipBoardTest {
     }
 
     @Test
-    void addComponent4(){
+    void addComponent4() {
         shipBoard.addComponent(new Cabin(new SideType[]{SideType.Universal, SideType.Special, SideType.Special, SideType.Universal}), 7, 8);
-        assertEquals(shipBoard.getShipBoardAttributes().getCrewMembers(),  2);
+        assertEquals(shipBoard.getShipBoardAttributes().getCrewMembers(), 2);
         shipBoard.removeComponent(7, 8);
-        assertEquals(shipBoard.getShipBoardAttributes().getCrewMembers(),  0);
+        assertEquals(shipBoard.getShipBoardAttributes().getCrewMembers(), 0);
     }
 
-    @Test
-    void addComponent5(){
-        shipBoard.addComponent();
-    }
+    //@Test
+    //void addComponent5(){
+    //  shipBoard.addComponent();
+//    }
 }
