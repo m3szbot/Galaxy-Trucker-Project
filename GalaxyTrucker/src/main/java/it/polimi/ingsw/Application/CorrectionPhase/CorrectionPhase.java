019fe218400e0d0c1cd7407b90d0f867d1d73073
Botond Meszaros <10874705@polimi.it>
2025-04-29 15:48:48 +0200

    update CorrectionPhase

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/Application/CorrectionPhase/CorrectionPhase.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/Application/CorrectionPhase/CorrectionPhase.java
@@ -1,4 +1,48 @@
 package it.polimi.ingsw.Application.CorrectionPhase;
 
-public class CorrectionPhase {
+import it.polimi.ingsw.Application.GameInformation;
+import it.polimi.ingsw.Application.Startable;
+import it.polimi.ingsw.Shipboard.Player;
+
+public class CorrectionPhase implements Startable {
+    CorrectionView correctionView;
+
+    public CorrectionPhase() {
+        correctionView = new CorrectionView();
+    }
+
+    /*
+    remove components until player ship is valid
+     */
+    // TODO: create threads for players
+    // TODO state pattern
+    // TODO refactor into methods
+    public void start(GameInformation gameInformation) {
+        boolean errors;
+        int col, row;
+        for (Player player : gameInformation.getPlayerList()) {
+            errors = checkErrors(player);
+            // ask to correct errors
+            while (errors) {
+                col = correctionView.promptForColumn();
+                row = correctionView.promptForRow();
+                player.getShipBoard().removeComponent(col, row, true);
+                checkErrors(player);
+            }
+            correctionView.printFinishedMessage();
+        }
+    }
+
+    private boolean checkErrors(Player player) {
+        boolean errors = false;
+        boolean[][] matr = player.getShipBoard().getMatrErrors();
+        for (int i = 0; i < 12 && !errors; i++) {
+            for (int j = 0; j < 12 && !errors; j++) {
+                if (matr[i][j])
+                    errors = true;
+            }
+        }
+        return errors;
+    }
+
 }
--- GalaxyTrucker/src/main/java/it/polimi/ingsw/Application/CorrectionPhase/CorrectionView.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/Application/CorrectionPhase/CorrectionView.java
@@ -1,4 +1,29 @@
 package it.polimi.ingsw.Application.CorrectionPhase;
 
+import java.util.Scanner;
+
 public class CorrectionView {
+
+    public void printFinishedMessage() {
+        System.out.println("Your ship is correct, please wait for other players to finish...");
+    }
+
+    /*
+    get column and return it
+     */
+    public int promptForColumn() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter column:");
+        return scanner.nextInt();
+    }
+
+    /*
+    get row and return it
+     */
+    public int promptForRow() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter row:");
+        return scanner.nextInt();
+    }
+
 }
