4f4232c816c643a658f9fa54a0167e3cbd6aef97
Botond Meszaros <10874705@polimi.it>
2025-04-29 15:30:43 +0200

    update EvaluationPhase

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/Application/EvaluationPhase/EvaluationView.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/Application/EvaluationPhase/EvaluationView.java
@@ -1,28 +1,14 @@
 package it.polimi.ingsw.Application.EvaluationPhase;
 
-import java.util.Scanner;
 
 public class EvaluationView {
 
-    public EvaluationView() {
-    }
-
-    public void printEvaluationMessage() {
-        System.out.println("EvaluationPhase: evaluating...");
-    }
-
-    public void showPlayerCredits(String message) {
+    /*
+    show credits of all players
+    same for all players
+    // TODO
+     */
+    public void printPlayerCreditsMessage(String message) {
         System.out.println(message);
-    }
-
-
-    // TODO change to state pattern
-    public boolean askAnotherGame(String message) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(message);
-        if (scanner.next().equals("y")) {
-            return true;
-        }
-        return false;
     }
 }
