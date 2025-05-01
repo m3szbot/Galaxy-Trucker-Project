3f35f126ddee59cbf58fa6270f909d727b0a36cd
Botond Meszaros <10874705@polimi.it>
2025-05-01 21:31:20 +0200

    update CorrectionPhase

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/Controller/CorrectionPhase/CorrectionPhase.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/Controller/CorrectionPhase/CorrectionPhase.java
@@ -5,51 +5,34 @@ import it.polimi.ingsw.Model.GameInformation.GameInformation;
 import it.polimi.ingsw.Model.ShipBoard.Player;
 import it.polimi.ingsw.View.CorrectionView.CorrectionView;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CorrectionPhase implements Startable {
-    CorrectionView correctionView;
+    Map<Player, CorrectionView> playerViewMap;
 
-    public CorrectionPhase() {
-        correctionView = new CorrectionView();
-    }
-
-    /**
-     * remove components until shipboard of player is valid
-     */
-    // TODO: create threads for players
-    // TODO state pattern
-    // TODO refactor into methods
-    public void start(GameInformation gameInformation) {
-        boolean errors;
-        int col, row;
+    public CorrectionPhase(GameInformation gameInformation) {
+        // create player-specific views
+        playerViewMap = new HashMap<>();
         for (Player player : gameInformation.getPlayerList()) {
-            errors = checkErrors(player);
-            // ask to correct errors
-            while (errors) {
-                col = correctionView.promptForColumn();
-                row = correctionView.promptForRow();
-                player.getShipBoard().removeComponent(col, row, true);
-                errors = checkErrors(player);
-            }
-            correctionView.printFinishedMessage();
+            playerViewMap.put(player, new CorrectionView());
         }
     }
 
     /**
-     * Checks if there are any incorrectly placed components in shipboard (errors)
+     * start player threads
      *
-     * @param player
-     * @return true if there are errors, false if shipboard is valid
+     * @param gameInformation
      */
-    private boolean checkErrors(Player player) {
-        boolean errors = false;
-        boolean[][] matr = player.getShipBoard().getMatrErrors();
-        for (int i = 0; i < 12 && !errors; i++) {
-            for (int j = 0; j < 12 && !errors; j++) {
-                if (matr[i][j])
-                    errors = true;
-            }
+    // TODO state pattern
+    public void start(GameInformation gameInformation) {
+        for (Player player : gameInformation.getPlayerList()) {
+            // thread variable is lost, but the thread itself is not
+            CorrectionThread playerThread = new CorrectionThread(player, playerViewMap.get(player));
+            playerThread.start();
+
         }
-        return errors;
     }
+
 
 }
---
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/Controller/CorrectionPhase/CorrectionThread.java
@@ -0,0 +1,53 @@
+package it.polimi.ingsw.Controller.CorrectionPhase;
+
+import it.polimi.ingsw.Model.ShipBoard.Player;
+import it.polimi.ingsw.View.CorrectionView.CorrectionView;
+
+public class CorrectionThread extends Thread {
+    Player player;
+    CorrectionView playerView;
+
+    public CorrectionThread(Player player, CorrectionView playerView) {
+        this.player = player;
+        this.playerView = playerView;
+    }
+
+    /**
+     * remove components until shipboard of player is valid
+     */
+    @Override
+    public void run() {
+        boolean errors;
+        int col, row;
+        // check if there are errors
+        errors = checkErrors(player);
+        // correct errors
+        while (errors) {
+            col = playerView.promptForColumn();
+            row = playerView.promptForRow();
+            // no check for col, row value - if out of bounds, nothing happens
+            player.getShipBoard().removeComponent(col, row, true);
+            errors = checkErrors(player);
+        }
+        // errors corrected
+        playerView.printFinishedMessage();
+    }
+
+    /**
+     * Checks if there are any incorrectly placed components (errors) in shipboard
+     *
+     * @param player
+     * @return true if there are errors, false if shipboard is valid
+     */
+    private boolean checkErrors(Player player) {
+        boolean errors = false;
+        boolean[][] matr = player.getShipBoard().getMatrErrors();
+        for (int i = 0; i < player.getShipBoard().getMatrixCols() && !errors; i++) {
+            for (int j = 0; j < player.getShipBoard().getMatrixRows() && !errors; j++) {
+                if (matr[i][j])
+                    errors = true;
+            }
+        }
+        return errors;
+    }
+}
