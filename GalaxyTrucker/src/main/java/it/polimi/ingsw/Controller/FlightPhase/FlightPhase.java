070b7ca49d6311e4229dd30452142cf5596747e2
Botond Meszaros <10874705@polimi.it>
2025-05-02 15:54:03 +0200

    update FLightPhase

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/Controller/FlightPhase/FlightPhase.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/Controller/FlightPhase/FlightPhase.java
@@ -3,27 +3,31 @@ package it.polimi.ingsw.Controller.FlightPhase;
 import it.polimi.ingsw.Controller.Game.Startable;
 import it.polimi.ingsw.Model.FlightBoard.FlightBoard;
 import it.polimi.ingsw.Model.GameInformation.GameInformation;
-import it.polimi.ingsw.Model.GameInformation.GameType;
+import it.polimi.ingsw.Model.ShipBoard.Player;
 import it.polimi.ingsw.View.FlightView.FlightView;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FlightPhase implements Startable {
+    Map<Player, FlightView> playerViewMap;
+
+    public FlightPhase(GameInformation gameInformation) {
+        playerViewMap = new HashMap<>();
+        // create player-specific flight views
+        for (Player player : gameInformation.getPlayerList()) {
+            playerViewMap.put(player, new FlightView(gameInformation));
+        }
+    }
 
     public void start(GameInformation gameInformation) {
-
+        // TODO use playerViewMap
         FlightView flightView = new FlightView(gameInformation);
         FlightBoard flightBoard = gameInformation.getFlightBoard();
-        int numberOfCards;
 
-        if (gameInformation.getGameType() == GameType.TestGame) {
-            numberOfCards = 8;
-        } else {
-            numberOfCards = 12;
-        }
-
-        for (int i = 0; i < numberOfCards; i++) {
-
+        while (flightBoard.getCardsNumber() > 0) {
+            // TODO pass playerViewMap so each user sees his own speicific view
             flightBoard.getNewCard().resolve(flightBoard, flightView);
-
         }
 
     }
