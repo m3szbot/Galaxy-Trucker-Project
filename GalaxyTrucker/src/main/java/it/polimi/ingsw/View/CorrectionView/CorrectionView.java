3f82f46095915901c84cf4496811bef6d1b5bd87
Botond Meszaros <10874705@polimi.it>
2025-05-12 19:14:38 +0200

    update correction views

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/View/CorrectionView/CorrectionView.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/View/CorrectionView/CorrectionView.java
@@ -1,21 +1,6 @@
 package it.polimi.ingsw.View.CorrectionView;
 
-import it.polimi.ingsw.Model.ShipBoard.Player;
-import it.polimi.ingsw.Model.ShipBoard.ShipBoard;
 import it.polimi.ingsw.View.GeneralView;
 
 public abstract class CorrectionView extends GeneralView {
-
-
-    // TODO: remove
-    public abstract void printErrorsMessage(ShipBoard shipBoard);
-
-    public abstract void printFinishedMessage();
-
-    public abstract int[] promptForColumnRow();
-
-    public abstract void printComponentRemovalMessage(int col, int row);
-
-    public abstract void printPlayerRemovalMessage(Player player);
-
 }
