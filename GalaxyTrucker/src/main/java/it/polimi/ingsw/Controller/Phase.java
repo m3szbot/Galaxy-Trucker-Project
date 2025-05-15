0d85b74a3065b64052647a9a72fc0de69a2f1c90
Botond Meszaros <10874705@polimi.it>
2025-05-15 15:13:36 +0200

    update Phase

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/Controller/Phase.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/Controller/Phase.java
@@ -22,25 +22,33 @@ public abstract class Phase {
         this.gameMessenger = ClientMessenger.getGameMessenger(gameInformation.getGameCode());
     }
 
+    public abstract void start();
+
     /**
-     * Sets the given gamePhase for both server (gameInformation) and client.
+     * Sets the given gamePhase for both server (gameInformation) and all clients.
      */
-    public void setGamePhaseServerClient(GamePhase gamePhase) {
+    public void setGamePhaseToAll(GamePhase gamePhase) {
         GameMessenger gameMessenger = ClientMessenger.getGameMessenger(gameInformation.getGameCode());
         // server
         gameInformation.setGamePhase(gamePhase);
         System.out.printf("%s phase is starting...\n", gamePhase);
-        // client
+        // clients
         gameMessenger.setGamePhaseToAll(gamePhase);
         gameMessenger.sendMessageToALl(String.format("%s phase is starting...\n", gamePhase));
     }
 
+    /**
+     * Disconnect player from the server.
+     */
     public void disconnectPlayer(Player player) {
         gameInformation.getPlayerList().remove(player);
         gameInformation.getDisconnectedPlayerList().add(player);
         ClientMessenger.getGameMessenger(gameInformation.getGameCode()).clearPlayerResources(player);
     }
 
+    /**
+     * Reconnect player to the server.
+     */
     public void reconnectPlayer(Player player) {
         gameInformation.getDisconnectedPlayerList().remove(player);
         gameInformation.getPlayerList().add(player);
