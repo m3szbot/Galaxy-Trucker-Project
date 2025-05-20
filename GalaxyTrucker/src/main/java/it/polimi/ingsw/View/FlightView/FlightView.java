41f79d81c4c86f642f4f0b8a09f25b819592b455
LudoMeloni <ludovico.meloni@mail.polimi.it>
2025-05-20 14:30:19 +0200

    Modifiche a classi test per correzione errori

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/View/FlightView/FlightView.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/View/FlightView/FlightView.java
@@ -15,22 +15,24 @@ public abstract class FlightView extends GeneralView {
         System.out.println("FlightBoard:\n");
 
         for (int i = flightBoard.getPlayerOrderList().size() - 1; i >= 0; i--) {
-            System.out.println("PlayerOrderList not empty");
 
+            //String used to print the nickName
             player = flightBoard.getPlayerOrderList().get(i).getNickName();
-            System.out.println("player order list accessible");
 
+            //Temp1 holds the current player tile, temp2 holds the subsequent player tile
             temp1 = flightBoard.getPlayerTile(flightBoard.getPlayerOrderList().get(i));
             temp2 = 0;
+
+            //If the current player is not the first, diff is used to print the number of tiles between the two players
             if (i != 0) {
                 temp2 = flightBoard.getPlayerTile(flightBoard.getPlayerOrderList().get(i - 1));
             }
             diff = temp2 - temp1;
 
             if (i == 0) {
-                System.out.printf("%s is 1st\n", player);
+                System.out.printf("%s is 1st\n\n", player);
             } else {
-                System.out.printf("%s---%d--->", player, diff);
+                System.out.printf("%s ---%d---> ", player, diff);
             }
         }
     }
