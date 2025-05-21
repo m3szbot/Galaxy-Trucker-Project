4ca8ca6784e5aafd669593437e61e1c7e7784b12
Botond Meszaros <10874705@polimi.it>
2025-05-21 22:26:01 +0200

    create ServerRemoteInterface

---
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/Connection/ServerSide/RMI/ServerRemoteInterface.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.Connection.ServerSide.RMI;
+
+import it.polimi.ingsw.Connection.ClientSide.ClientServerInvokableMethods;
+import it.polimi.ingsw.View.ViewServerInvokableMethods;
+
+public interface ServerRemoteInterface extends ViewServerInvokableMethods, ClientServerInvokableMethods {
+}
