e22174059d5ca647fabd0aae1cebe285990351dd
Botond Meszaros <10874705@polimi.it>
2025-05-21 20:03:22 +0200

    update PlayerGameMessenger

---
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/Connection/ServerSide/PlayerGameMessenger.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.Connection.ServerSide;
+
+import it.polimi.ingsw.Connection.ClientSide.ClientServerInvokableMethods;
+import it.polimi.ingsw.View.ViewServerInvokableMethods;
+
+public abstract class PlayerGameMessenger implements ViewServerInvokableMethods, ClientServerInvokableMethods {
+}
---
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/Connection/ServerSide/PlayerGameMessengerSocket.java
@@ -0,0 +1,4 @@
+package it.polimi.ingsw.Connection.ServerSide;
+
+public abstract class PlayerGameMessengerSocket extends PlayerGameMessenger {
+}
