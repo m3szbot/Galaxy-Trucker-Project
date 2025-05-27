ef6641636da7e9b6f0e64b46ff59e60d8dfd2aa4
GeckyMerlo <giacomo.merlo@mail.polimi.it>
2025-05-27 18:38:28 +0200

    update

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/View/AssemblyView/AssemblyViewGUI.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/View/AssemblyView/AssemblyViewGUI.java
@@ -3,6 +3,8 @@ package it.polimi.ingsw.View.AssemblyView;
 import it.polimi.ingsw.Model.Components.Component;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -10,6 +12,8 @@ import java.util.concurrent.atomic.AtomicReference;
 public abstract class AssemblyViewGUI extends AssemblyView {
     private transient AtomicReference<String> userInput;
     String input;
+    @FXML
+    private ImageView inUseComponent;
 
     AssemblyViewGUI(AtomicReference<String> userInput) {
         this.userInput = userInput;
@@ -24,12 +28,25 @@ public abstract class AssemblyViewGUI extends AssemblyView {
         input = "draw";
         this.userInput.set(input);
     }
-/*
-    @Override
-    public void public void printComponent(Component component) {
-        String componentString = getCorrectComponentString(component);
-        System.out.printf(componentString);
-    }*/
+    @FXML
+    public void placeInput(ActionEvent event){
+        input = "place";
+        this.userInput.set(input);
+        //... logica rinoscimento id e place
+    }
+
+    @FXML
+    public void rotateInput(ActionEvent event){
+        input = "rotate";
+        this.userInput.set(input);
+    }
+
+   @Override
+    public void printComponent(Component component) {
+       // String imdAddress = component.getImgAddress();
+      //  Image img = new Image(imdAddress);
+       // inUseComponent.setImage(img);
+    }
 
 
 
