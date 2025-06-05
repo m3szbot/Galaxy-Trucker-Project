138a719a46dc751a149fcbcf878a90ee1d7c6123
Carlo Maggi <tempo.drago@gmail.com>
2025-06-05 19:09:50 +0200

    Defining general controller

---
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/View/GUI/GeneralController.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.View.GUI;
+
+/**
+ * The general controller class set the gui for each phase by appending the
+ * relative fxml file to the General fxml file. The set is happening in the client
+ * side.
+ */
+
+public class GeneralController {
+}
