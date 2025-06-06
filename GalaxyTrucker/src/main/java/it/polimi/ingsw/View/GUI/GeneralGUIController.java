b0e5a8dd41ab7a14c4ccd8951683e50021b2c86a
Carlo Maggi <tempo.drago@gmail.com>
2025-06-06 15:27:33 +0200

    Advancing in GUI development

---
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/View/GUI/GeneralGUIController.java
@@ -0,0 +1,91 @@
+package it.polimi.ingsw.View.GUI;
+
+import it.polimi.ingsw.Model.GameInformation.GamePhase;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+/**
+ * The general controller class set the gui for each phase by appending the
+ * relative fxml file to the General fxml file. The set is happening in the client
+ * side.
+ *
+ * @author carlo
+ */
+
+public class GeneralGUIController {
+
+    @FXML
+    private AnchorPane rootPane;
+
+    @FXML
+    public void initialize(){
+
+        loadPhaseGUI("/fxml/LobbyView/LobbyView.fxml");
+
+    }
+
+    /**
+     * Set the gui interface for the phase passed as parameter. The method must
+     * be used in the client side in the setGamePhase method
+     * @param gamePhase
+     */
+
+    public void setPhaseGUI(GamePhase gamePhase){
+
+        switch (gamePhase){
+            case Assembly -> {
+
+                loadPhaseGUI("/fxml/AssemblyView/AssemblyView.fxml");
+
+            }
+            case Correction -> {
+
+                loadPhaseGUI("/fxml/CorrectionView/CorrectionView.fxml");
+
+            }
+            case Flight -> {
+
+                loadPhaseGUI("/fxml/FlightView/FlightView.fxml");
+
+            }
+            case Evaluation -> {
+
+                loadPhaseGUI("/fxml/EvaluationView/EvaluationView.fxml");
+
+            }
+        }
+
+    }
+
+    private void loadPhaseGUI(String fxmlPath){
+
+        try {
+
+            Node node = FXMLLoader.load(getClass().getResource(fxmlPath));
+
+            //removing the old node
+
+            rootPane.getChildren().clear();
+
+            //adding the new node
+            rootPane.getChildren().add(node);
+
+            //making the inserted node occupy the entire stage
+
+            AnchorPane.setTopAnchor(node, 0.0);
+            AnchorPane.setBottomAnchor(node, 0.0);
+            AnchorPane.setLeftAnchor(node, 0.0);
+            AnchorPane.setRightAnchor(node, 0.0);
+
+        } catch (IOException e) {
+
+            System.err.println("Error while loading the fxml file at path: " + fxmlPath);
+
+        }
+    }
+
+}
--- GalaxyTrucker/src/main/java/it/polimi/ingsw/View/GUILoader.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/View/GUILoader.java
@@ -1,20 +1,31 @@
 package it.polimi.ingsw.View;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class GUILoader extends Application {
-    GeneralView view;
-    public GUILoader(GeneralView view) {
-        this.view = view;
-    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        try{
 
+            Parent galaxyTruckerGui = FXMLLoader.load(getClass().getResource("/fxml/GeneralFXML.fxml"));
+            primaryStage.setTitle("Galaxy Trucker");
+            primaryStage.setScene(new Scene(galaxyTruckerGui));
+            primaryStage.setFullScreen(true);
+            primaryStage.show();
+
+
+        }catch (IOException e){
+           System.err.println("Error while loading gui");
+        }
 
     }
-
 
 }
