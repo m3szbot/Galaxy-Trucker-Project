db1cdcd65e246422c49acbcd8bc49f6d4818513a
Carlo Maggi <tempo.drago@gmail.com>
2025-06-10 19:40:34 +0200

    Defining IndexChecker

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/Controller/FlightPhase/IndexChecker.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/Controller/FlightPhase/IndexChecker.java
@@ -1,8 +1,15 @@
 package it.polimi.ingsw.Controller.FlightPhase;
 
+import it.polimi.ingsw.Model.GameInformation.GameInformation;
+import it.polimi.ingsw.Model.ShipBoard.Player;
+
+import java.util.List;
+
 /**
  * Util which check for the for loop index at the beginning of
  * it every time. Realign the index based on the disconnected players.
+ *
+ * @author carlo
  *
  */
 
@@ -15,11 +22,24 @@ public class IndexChecker {
      * @return the index corrected
      */
 
-    /*
     public static int checkIndex(GameInformation gameInformation, int currentIndex){
-        //TODO (LUDO)
+
+        List<Player> disconnectedPlayer = gameInformation.getDisconnectedPlayerList();
+        List<Player> flightBoardPlayersList = gameInformation.getFlightBoard().getPlayerOrderList();
+        int updatedIndex = currentIndex;
+
+        for(int i = 0; i <= currentIndex; i++){
+
+            if(disconnectedPlayer.contains(flightBoardPlayersList.get(i))){
+               updatedIndex--;
+            }
+
+        }
+
+        return updatedIndex;
+
+
     }
 
-     */
 
 }
