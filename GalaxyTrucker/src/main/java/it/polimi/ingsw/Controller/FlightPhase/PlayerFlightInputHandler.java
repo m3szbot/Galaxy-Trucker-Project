038673359066cf3cb85b509ba6146aa926066e28
Botond Meszaros <10874705@polimi.it>
2025-06-20 16:26:14 +0200

    adjust player threads

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/Controller/FlightPhase/PlayerFlightInputHandler.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/Controller/FlightPhase/PlayerFlightInputHandler.java
@@ -17,28 +17,27 @@ public class PlayerFlightInputHandler {
     private static Map<Player, InputThread> playerInputThreadMap = new HashMap<>();
 
     /**
-     *
      * @param player
      * @return true if the input thread of the related player is present, false otherwise.
      */
 
-    public static boolean checkInputThreadActivity(Player player){
-       if(playerInputThreadMap.containsKey(player)){
-           return true;
-       }
-       else {
-           return false;
-       }
+    public static boolean checkInputThreadActivity(Player player) {
+        if (playerInputThreadMap.containsKey(player)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
      * adds a new player to the handler memory. The player input thread is also
      * started
+     *
      * @param player
      * @param gameInformation
      */
 
-    public static void addPlayer(Player player, GameInformation gameInformation){
+    public static void addPlayer(Player player, GameInformation gameInformation) {
 
         InputThread inputThread = new InputThread(player, gameInformation);
         playerInputThreadMap.put(player, inputThread);
@@ -48,10 +47,11 @@ public class PlayerFlightInputHandler {
     /**
      * Stops the inputThread of the player. To call if a disconnection occur or if the flight
      * phase has ended.
+     *
      * @param player
      */
 
-    public static void removePlayer(Player player){
+    public static void removePlayer(Player player) {
 
         InputThread inputThread = playerInputThreadMap.get(player);
         inputThread.endThread();
@@ -61,21 +61,31 @@ public class PlayerFlightInputHandler {
 
     /**
      * To be called when the turn of the player has started
+     *
      * @param player
      */
 
-    public static void startPlayerTurn(Player player){
-       InputThread inputThread = playerInputThreadMap.get(player);
-       inputThread.setPlayerTurn(true);
+    public static void startPlayerTurn(Player player) {
+        InputThread inputThread = playerInputThreadMap.get(player);
+
+        if (inputThread.getIsPlayerTurn())
+            throw new IllegalStateException(("Error: player turn already started"));
+
+        inputThread.setPlayerTurn(true);
     }
 
     /**
      * To be called when the player turn ends
+     *
      * @param player
      */
 
-    public static void endPlayerTurn(Player player){
+    public static void endPlayerTurn(Player player) {
         InputThread inputThread = playerInputThreadMap.get(player);
+
+        if (!inputThread.getIsPlayerTurn())
+            throw new IllegalStateException(("Error: player turn already ended"));
+
         inputThread.setPlayerTurn(false);
     }
 
