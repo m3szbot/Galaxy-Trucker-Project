a025d8b4223501e0702afc22ce6999fdfda1538b
Carlo Maggi <tempo.drago@gmail.com>
2025-06-23 15:32:03 +0200

    Finishing GUI. Only AssemblyPhase has to be completed

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/View/GUI/EvaluationControllers/EvaluationGUIController.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/View/GUI/EvaluationControllers/EvaluationGUIController.java
@@ -1,7 +1,59 @@
 package it.polimi.ingsw.View.GUI.EvaluationControllers;
 
-/*
-public class EvaluationGUIController extends GUIController implements PlayerInputSetter{
+import it.polimi.ingsw.View.GUI.GUIController;
+import it.polimi.ingsw.View.GUI.PlayerInputSetter;
+import it.polimi.ingsw.View.GUI.utils.FXUtil;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+public class EvaluationGUIController extends GUIController implements PlayerInputSetter {
+
+    @FXML
+    TextArea console;
+
+    @FXML
+    Button button;
+
+
+    public void initialize(){
+
+        button.setOnAction(e -> {
+            Platform.exit();
+        });
+
+    }
+
+
+    public void refreshConsole(String message){
+        FXUtil.runOnFXThread(() -> console.appendText(message + "\n"));
+    }
+
+
+    public void refreshShipBoard(Node node){
+
+        //not used
+
+    }
+
+    public void refreshCard(Node node){
+
+        //not used
+
+    }
+
+    public void refreshComponent(Node node){
+
+        //not used
+
+    }
+
+    public void refreshFlightBoard(Node node){
+
+        //not used
+
+    }
 }
 
- */
