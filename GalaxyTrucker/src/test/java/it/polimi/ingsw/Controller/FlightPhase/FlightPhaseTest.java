0a9b5382cb6e37258ba42c0be91cca8d0a30c5c8
Botond Meszaros <10874705@polimi.it>
2025-06-25 20:03:43 +0200

    update tests

--- GalaxyTrucker/src/test/java/it/polimi/ingsw/Controller/FlightPhase/FlightPhaseTest.java
+++ GalaxyTrucker/src/test/java/it/polimi/ingsw/Controller/FlightPhase/FlightPhaseTest.java
@@ -1,42 +1,39 @@
 package it.polimi.ingsw.Controller.FlightPhase;
 
-import it.polimi.ingsw.Model.GameInformation.GameInformation;
-import it.polimi.ingsw.Model.GameInformation.GameType;
-import it.polimi.ingsw.Model.ShipBoard.Color;
+import it.polimi.ingsw.Controller.Game.Game;
+import it.polimi.ingsw.Mocker;
+import it.polimi.ingsw.Model.FlightBoard.FlightBoard;
+import it.polimi.ingsw.Model.IllegalSelectionException;
 import it.polimi.ingsw.Model.ShipBoard.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class FlightPhaseTest {
-    GameInformation gameInformation;
-    Player playerA, playerB, playerC, playerD;
-    /*
-    initialize flightPhase only after setting simulated input!
-    creates flightView and scanner in it!
-     */
+    Game game;
     FlightPhase flightPhase;
+    Player player;
+    FlightBoard flightBoard;
+
 
     @BeforeEach
-    void setUp() {
-        // set up gameInformation
-        gameInformation = new GameInformation();
-        gameInformation.setGameType(GameType.NORMALGAME);
-        gameInformation.setUpGameInformation(GameType.NORMALGAME, 4);
-        // add players
-        playerA = new Player("A", Color.BLUE, gameInformation);
-        playerB = new Player("B", Color.RED, gameInformation);
-        playerC = new Player("C", Color.YELLOW, gameInformation);
-        playerD = new Player("D", Color.GREEN, gameInformation);
-        gameInformation.setMaxNumberOfPlayers(4);
-        gameInformation.addPlayer(playerA);
-        gameInformation.addPlayer(playerB);
-        gameInformation.addPlayer(playerC);
-        gameInformation.addPlayer(playerD);
+    void setup() throws IllegalSelectionException {
+        game = Mocker.mockNormalGame1Player();
+        flightPhase = game.getFlightPhase();
+        player = game.getGameInformation().getPlayerList().getFirst();
+        flightBoard = game.getGameInformation().getFlightBoard();
+        flightBoard.addPlayer(player, 1);
 
+        assertTrue(flightBoard.isInFlight(player));
     }
 
     @Test
-    public void start() {
-        flightPhase = new FlightPhase(gameInformation);
+    void timeoutNoInput() {
+        flightPhase.start();
+
+        assertEquals(0, game.getGameInformation().getPlayerList().size());
+        assertFalse(flightBoard.isInFlight(player));
+
     }
 }
