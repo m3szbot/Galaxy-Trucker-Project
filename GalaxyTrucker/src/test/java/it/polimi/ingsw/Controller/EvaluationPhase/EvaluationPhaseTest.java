39973b8a23c1c061713f50800da354f514fcf992
Botond Meszaros <10874705@polimi.it>
2025-06-27 23:28:24 +0200

    complete evaluation test

--- GalaxyTrucker/src/test/java/it/polimi/ingsw/Controller/EvaluationPhase/EvaluationPhaseTest.java
+++ GalaxyTrucker/src/test/java/it/polimi/ingsw/Controller/EvaluationPhase/EvaluationPhaseTest.java
@@ -2,17 +2,21 @@ package it.polimi.ingsw.Controller.EvaluationPhase;
 
 import it.polimi.ingsw.Controller.Game.Game;
 import it.polimi.ingsw.Mocker;
+import it.polimi.ingsw.Model.FlightBoard.FlightBoard;
 import it.polimi.ingsw.Model.IllegalSelectionException;
 import it.polimi.ingsw.Model.ShipBoard.Player;
 import it.polimi.ingsw.Model.ShipBoard.ShipBoard;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class EvaluationPhaseTest {
     Game game;
     EvaluationPhase evaluationPhase;
     Player player;
     ShipBoard shipBoard;
+    FlightBoard flightBoard;
 
     @BeforeEach
     void setup() {
@@ -21,6 +25,7 @@ class EvaluationPhaseTest {
         player = Mocker.getFirstPlayer();
         shipBoard = player.getShipBoard();
         evaluationPhase = game.getEvaluationPhase();
+        flightBoard = Mocker.getGameInformation().getFlightBoard();
     }
 
     @Test
@@ -33,15 +38,52 @@ class EvaluationPhaseTest {
 
     @Test
     void test1PlayerOnFlightBoard() throws IllegalSelectionException {
-        game.getGameInformation().getFlightBoard().addPlayer(player, 1);
+        flightBoard.addPlayer(player, 1);
         try {
             evaluationPhase.start();
         } catch (Exception e) {
         }
+
+        assertEquals(12, shipBoard.getShipBoardAttributes().getCredits());
     }
 
     @Test
-    void addGainedCredtisToExistingCredits() {
+    void addGainedCreditsToExistingCredits() throws IllegalSelectionException {
+        shipBoard.getShipBoardAttributes().addCredits(10);
+        flightBoard.addPlayer(player, 1);
+        try {
+            evaluationPhase.start();
+        } catch (Exception e) {
+        }
 
+        // 10 + 12
+        assertEquals(22, shipBoard.getShipBoardAttributes().getCredits());
+    }
+
+    @Test
+    void eliminatedPlayer() throws IllegalSelectionException {
+        flightBoard.addPlayer(player, 1);
+        flightBoard.eliminatePlayer(player);
+        try {
+            evaluationPhase.start();
+        } catch (Exception e) {
+        }
+
+        // DNF
+        assertEquals(0, shipBoard.getShipBoardAttributes().getCredits());
+    }
+
+    @Test
+    void disconnectedPlayer() throws IllegalSelectionException {
+        flightBoard.addPlayer(player, 1);
+        flightBoard.eliminatePlayer(player);
+        game.getGameInformation().disconnectPlayerInGameInformation(player);
+        try {
+            evaluationPhase.start();
+        } catch (Exception e) {
+        }
+
+        // DNF
+        assertEquals(0, shipBoard.getShipBoardAttributes().getCredits());
     }
 }
