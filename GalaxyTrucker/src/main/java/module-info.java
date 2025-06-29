42346764d13dc9994d3846efe860a436f5d30f0a
Carlo Maggi <tempo.drago@gmail.com>
2025-06-29 09:51:58 +0200

    Adding module to produce jar files

---
+++ GalaxyTrucker/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module it.polimi.ingsw {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires com.fasterxml.jackson.databind;
+    requires java.rmi;
+
+    exports it.polimi.ingsw.Connection;
+    exports it.polimi.ingsw.Controller;
+    exports it.polimi.ingsw.Model;
+    exports it.polimi.ingsw.View;
+}
